00cd38ad4d49dae0bcbf198416a7d3a8cb21b478
kaustub1234 <kaustubshetty123@gmail.com>
2023-06-13 00:55:01 +0530

    second commit

---
+++ Arrays/BestTimeToBuyAndSell.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    int maxPro = 0;
+    int n = prices.size();
+    int minPrice = INT_MAX;
+
+    for (int i = 0; i < prices.size(); i++) {
+        minPrice = min(minPrice, prices[i]);
+        maxPro = max(maxPro, prices[i] - minPrice);
+    }
+    
+    return maxPro;
+}
