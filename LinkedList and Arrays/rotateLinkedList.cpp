a062a075d660e5143c3921c49a8a2658469a5f83
kaustub1234 <kaustubshetty123@gmail.com>
2023-06-19 19:14:36 +0530

    LinkedList and arrays

---
+++ LinkedList and Arrays/rotateLinkedList.cpp
@@ -0,0 +1,20 @@
+node* rotateRight(node* head,int k) {
+    if(head == NULL||head->next == NULL||k == 0) return head;
+    //calculating length
+    node* temp = head;
+    int length = 1;
+    while(temp->next != NULL) {
+        ++length;
+        temp = temp->next;
+    }
+    //link last node to first node
+    temp->next = head;
+    k = k%length; //when k is more than length of list
+    int end = length-k; //to get end of the list
+    while(end--) temp = temp->next;
+    //breaking last node link and pointing to NULL
+    head = temp->next;
+    temp->next = NULL;
+        
+    return head;
+}
