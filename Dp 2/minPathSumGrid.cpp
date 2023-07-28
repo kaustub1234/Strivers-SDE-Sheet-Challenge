375d8fe463febc96013153771289679880708a45
kaustub1234 <kaustubshetty123@gmail.com>
2023-07-29 00:22:32 +0530

    bt, dp, dp2

---
+++ Dp 2/minPathSumGrid.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int minSumPath(int n, int m, vector<vector<int> > &matrix){
+      vector<int> prev(m,0);
+    for(int i=0; i<n ; i++){
+        vector<int> temp(m,0);
+        for(int j=0; j<m; j++){
+            if(i==0 && j==0) temp[j] = matrix[i][j];
+            else{
+                
+                int up = matrix[i][j];
+                if(i>0) up += prev[j];
+                else up += 1e9;
+                
+                int left = matrix[i][j];
+                if(j>0) left+=temp[j-1];
+                else left += 1e9;
+                
+                temp[j] = min(up,left);
+            }
+        }
+        prev=temp;
+    }
+    
+    return prev[m-1];
+    
+}
